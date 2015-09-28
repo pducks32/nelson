c6f6b803bd30ee57b5478dee7037bb6c329f2247
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-09-28 16:09:22 -0700

    refactor calling sub expressions

--- lib/nelson/expression.rb
+++ lib/nelson/expression.rb
@@ -4,19 +4,26 @@ module Nelson
     attr_accessor :terms
 
     def initialize(*terms)
+      raise ArgumentError, "At least 1 term must be specified" unless terms.length > 0
       @terms = terms
     end
 
     def *(term)
       rhs_value = self.call
-      lhs_value = term.respond_to?(:call) ? term.call : term
+      lhs_value = try_to_eval term
       MultipicationExpression.new(rhs_value, lhs_value).call
     end
 
     def +(term)
       rhs_value = self.call
-      lhs_value = term.respond_to?(:call) ? term.call : term
+      lhs_value = try_to_eval term
       AdditionExpression.new(rhs_value, lhs_value).call
+    end
+
+    private
+
+    def try_to_eval(term)
+      term.respond_to?(:call) ? term.call : term
     end
   end
 
