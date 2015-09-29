c40468e595d2486930f507e023d716f316a83554
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-09-28 18:34:00 -0700

    add subtraction builder methods to expression builders

--- lib/nelson/expression_builder.rb
+++ lib/nelson/expression_builder.rb
@@ -20,8 +20,13 @@ module Nelson
     def plus(term)
       AdditionExpressionBuilder.new(self, term)
     end
+
+    def minus(term)
+      SubtractionExpressionBuilder.new(self, term)
+    end
   end
 
   autoload :MultipicationExpressionBuilder, "nelson/expression_builders/multipication_expression_builder"
   autoload :AdditionExpressionBuilder, "nelson/expression_builders/addition_expression_builder"
+  autoload :SubtractionExpressionBuilder, "nelson/expressions/subtraction_expression_builder"
 end
