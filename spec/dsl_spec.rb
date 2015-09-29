2e9f2495b3ccf24070b4651f3ee9feac9cba3c1f
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-09-28 21:21:37 -0700

    add difference_of specs

--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -18,7 +18,17 @@ module Nelson
         it { is_expected.to be_a AdditionExpressionBuilder }
       end
       context "with 0 terms passed" do
-        it { expect { DSL.product_of() }.to raise_error ArgumentError }
+        it { expect { DSL.sum_of() }.to raise_error ArgumentError }
+      end
+    end
+
+    describe "#difference_of" do
+      context "with >= 1 terms passed" do
+        subject { DSL.difference_of(3, 4, 5) }
+        it { is_expected.to be_a SubtractionExpressionBuilder }
+      end
+      context "with 0 terms passed" do
+        it { expect { DSL.difference_of() }.to raise_error ArgumentError }
       end
     end
   end
