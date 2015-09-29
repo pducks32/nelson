58c95bca336c6a90bcb6d5eb60cf1647e2078fdb
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-09-28 21:20:54 -0700

    fix files in the wrong folders

--- lib/nelson/expression_builders/multipication_expression_builder.rb
+++ lib/nelson/expression_builders/multipication_expression_builder.rb
@@ -1,19 +1,9 @@
 
 module Nelson
-  class MultipicationExpression < Expression
-    def call
-      terms.reduce(:*)
-    end
-
-    def to_s
-      terms.sort_by { |t| t.is_a?(Expression) ? 1 : 0 }.reduce("") do |c, t|
-        case t
-        when Expression
-          c << "(#{t})"
-        else
-          c = [c, t].map(&:to_s).reject(&:empty?).join("*")
-        end
-      end
+  class MultipicationExpressionBuilder < ExpressionBuilder
+    def build
+      terms.map! { |e| e.is_a?(ExpressionBuilder) ? e.build : e }
+      MultipicationExpression.new(*terms)
     end
   end
 end
