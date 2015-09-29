80d97548b31cf72b25ec50b0a01f04240d589053
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-09-28 18:33:14 -0700

    add subtraction expression

---
+++ lib/nelson/expressions/subtraction_expression.rb
@@ -0,0 +1,15 @@
+
+module Nelson
+  class SubtractionExpression < Expression
+    def call
+      terms.reduce(:-)
+    end
+
+
+    def to_s
+      terms.map do |t|
+        t.is_a?(Expression) ? "(#{t})" : t
+      end.join("-")
+    end
+  end
+end
