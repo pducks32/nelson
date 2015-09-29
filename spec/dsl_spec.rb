22c104aa4045d5c6d14bc2e7dfe1005ca5b97089
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-09-28 21:22:52 -0700

    add division dsl specs

--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -12,6 +12,19 @@ module Nelson
       end
     end
 
+    describe "#quotient_of" do
+      context "with 2 terms passed" do
+        subject { DSL.quotient_of(3, 4) }
+        it { is_expected.to be_a DivisionExpressionBuilder }
+      end
+      context "with 0 terms passed" do
+        it { expect { DSL.quotient_of() }.to raise_error ArgumentError }
+      end
+      context "with > 2 terms passed" do
+        it { expect { DSL.quotient_of(3, 4, 5) }.to raise_error ArgumentError }
+      end
+    end
+
     describe "#sum_of" do
       context "with >= 1 terms passed" do
         subject { DSL.sum_of(3, 4, 5) }
