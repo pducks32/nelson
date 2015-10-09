aecef3143e57bfc61d6c028f0e87d612e10bf043
Patrick Metcalfe <git@patrickmetcalfe.com>
2015-10-08 17:30:50 -0700

    add expression with terms spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'nelson'
+
+RSpec::Matchers.define :be_an_expression_with_terms do |*expected|
+  match do |actual|
+    actual_terms = actual.terms
+    expected.all? { |e| actual_terms.include? e }
+  end
+end
